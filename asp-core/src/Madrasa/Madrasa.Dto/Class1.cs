7549190afdd7971155eb6be0c6122332f5bef09e
HokaHelal <eng.mah.ali@gmail.com>
2020-12-31 19:44:03 +0200

    Add AutoMapper & Automapper profile

--- asp-core/src/Madrasa/Madrasa.Dto/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Madrasa.Dto
-{
-    public class Class1
-    {
-    }
-}
---
+++ asp-core/src/Madrasa/Madrasa.Dto/User/NewStudentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madrasa.Dto
+{
+    public class NewStudentDto
+    {
+        public string name { get; set; }
+        public string username { get; set; }
+        public string photoUrl { get; set; }
+    }
+}
