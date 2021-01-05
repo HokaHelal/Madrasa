031e9e5689648155ea03dc5abf8b8e7e1ccbf16d
HokaHelal <eng.mah.ali@gmail.com>
2021-01-05 09:11:50 +0200

    display user and img of logged user in sidebar

--- angular/src/app/theme/sidebar/sidebar.component.ts
+++ angular/src/app/theme/sidebar/sidebar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs/operators';
+import { User } from 'src/app/_models/user';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  user: User;
 
-  constructor() { }
-
+  constructor(private accountService: AuthService, private toastr: ToastrService) { 
+    this.accountService.currentUser$.pipe(take(1)).subscribe(usr => this.user = usr);
+  }
   ngOnInit() {
   }
 
