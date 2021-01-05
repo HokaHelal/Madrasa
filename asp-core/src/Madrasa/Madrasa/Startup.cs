7960f439e350adfdbd3a74a7f7616e794b59261a
HokaHelal <eng.mah.ali@gmail.com>
2021-01-06 00:30:57 +0200

    add angular https to allowed origin

--- asp-core/src/Madrasa/Madrasa/Startup.cs
+++ asp-core/src/Madrasa/Madrasa/Startup.cs
@@ -40,7 +40,7 @@ namespace Madrasa.API
             app.UseCors(x => x.AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials()
-                .WithOrigins("http://localhost:4200"));
+                .WithOrigins("https://localhost:4200"));
 
             app.UseRouting();
 
