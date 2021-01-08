c73cb4e3cbf9e4a29a00e178df653719f8a64784
HokaHelal <eng.mah.ali@gmail.com>
2021-01-09 00:13:59 +0200

    fix latest topics

--- angular/src/app/forum/forum-main/forum-main.component.ts
+++ angular/src/app/forum/forum-main/forum-main.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { take } from 'rxjs/operators';
 import { Forum } from 'src/app/_models/Forum';
+import { TopicMain } from 'src/app/_models/TopicMain';
 import { User } from 'src/app/_models/user';
 import { AuthService } from 'src/app/_services/auth.service';
 import { ForumService } from 'src/app/_services/forum.service';
@@ -15,7 +16,7 @@ export class ForumMainComponent implements OnInit {
 
   user: User;
   forumSections: Forum[];
-  latestTopics: Forum;
+  latestTopics: TopicMain[];
   constructor(private accountService: AuthService, private route: ActivatedRoute) { 
   }
 
@@ -23,7 +24,6 @@ export class ForumMainComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.forumSections = data.forumData;
       this.latestTopics = data.latestTopics;
-      console.log(this.latestTopics);
     });
   }
 }
