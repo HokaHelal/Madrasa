d086d10057ab94ad86c3968e4b15a933b50fd001
HokaHelal <eng.mah.ali@gmail.com>
2021-01-10 23:33:48 +0200

    fix add topic validation

--- angular/src/app/forum/add-topic-modal/add-topic-modal.component.ts
+++ angular/src/app/forum/add-topic-modal/add-topic-modal.component.ts
@@ -1,13 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 import { take } from 'rxjs/operators';
 import { NewTopic } from 'src/app/_models/NewTopic';
 import { SectionMain } from 'src/app/_models/SectionMain';
-import { TopicMain } from 'src/app/_models/TopicMain';
 import { User } from 'src/app/_models/user';
 import { AuthService } from 'src/app/_services/auth.service';
 import { ForumService } from 'src/app/_services/forum.service';
+import { EditorComponent } from '../editor/editor.component';
 
 @Component({
   selector: 'app-add-topic-modal',
@@ -15,14 +16,17 @@ import { ForumService } from 'src/app/_services/forum.service';
   styleUrls: ['./add-topic-modal.component.scss']
 })
 export class AddTopicModalComponent implements OnInit {
+  @ViewChild('editor', {static: true}) editorComponent: EditorComponent; 
 
   constructor(public bsModalRef: BsModalRef,
      private userService: AuthService,
+     private fb: FormBuilder,
      private router: Router,
      private forumService: ForumService) {
     this.userService.currentUser$.pipe(take(1)).subscribe(usr => this.user = usr);
    }
 
+  newTopicForm: FormGroup;
   sections: SectionMain[];
   @Input() selectedSection: number = 0;
   user: User;
@@ -31,8 +35,11 @@ export class AddTopicModalComponent implements OnInit {
 
   ngOnInit() {
     this.initControls();
+    this.newTopicForm = this.fb.group({
+      title: ['', Validators.required],
+      section: ['', Validators.required]
+    })
   }
-
   initControls() {
     this.forumService.getDropDownSections(this.user.classId).subscribe((sections: SectionMain[]) => {
       this.sections = sections;
@@ -40,18 +47,22 @@ export class AddTopicModalComponent implements OnInit {
         this.selectedSection = sections[0].id;
       }
     });
+    console.log(this.editorComponent);
   }
 
-  addTopic(editorContent: string) {      
+  get isFormsValid() {
+    return this.newTopicForm.valid && this.editorComponent.editorForm.valid;
+  }
+  
+  addTopic() {      
       let newTopic: NewTopic = {
         title: this.title,
         authorId: this.user.userId,
-        content: editorContent,
+        content: this.editorComponent.content,
         sectionId: this.selectedSection,
         isPinned: this.isPinned
       };
 
-      console.log(newTopic);
       this.forumService.addTopic(newTopic).subscribe((topicId : number) => {
           this.router.navigateByUrl('/app/forums/thread/' + topicId);
           this.bsModalRef.hide();
--- angular/src/app/forum/editor/editor.component.ts
+++ angular/src/app/forum/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import 'quill-emoji';
 
@@ -9,61 +9,36 @@ import 'quill-emoji';
 })
 export class EditorComponent implements OnInit {
   previewReply = '';
-  editForm: FormGroup;
-  @Output() Submit = new EventEmitter();
-
+  editorForm: FormGroup;
+  
   modules = {
-    toolbar: {
-      container: [
-        ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-        ['blockquote', 'code-block'],
-
-        [{ header: 1 }, { header: 2 }], // custom button values
-        [{ list: 'ordered' }, { list: 'bullet' }],
-        [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
-        [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-        [{ direction: 'rtl' }], // text direction
-
-        [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
-        [{ header: [1, 2, 3, 4, 5, 6, false] }],
-
-        [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-        [{ font: [] }],
-        [{ align: [] }],
-
-        ['clean'], // remove formatting button
-
-        ['link', 'image', 'video'], // link and image, video
-        ['emoji'],
-      ],
-       handlers: { emoji: function () {} },
-      
+    toolbar: {      
+       handlers: { emoji: function () {} },      
     },
     'emoji-toolbar': true,
     'emoji-textarea': true,
     'emoji-shortname': true,
   };
 
-  constructor() {}
+  constructor() {
+  }
 
   ngOnInit() {
-    this.editForm = new FormGroup({
-      editor: new FormControl('', [Validators.required]),
+    this.editorForm = new FormGroup({
+      editor: new FormControl(''),
     });
   }
 
   onPreview() {
-    this.previewReply = this.editForm.get('editor').value;
+    this.previewReply = this.editorForm.get('editor').value;
   }
 
   onClear() {
-    this.editForm.get('editor').setValue('');
+    this.editorForm.get('editor').setValue('');
     this.previewReply = '';
   }
 
-  onSubmit() {
-    const editorValue = this.editForm.get('editor').value;
-    this.Submit.emit(editorValue);
-    this.onClear();
+  get content() {
+    return this.editorForm.get('editor').value;
   }
 }
